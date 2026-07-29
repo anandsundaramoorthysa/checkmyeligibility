3033a7e645b3f031873c4b2b52ba9cab179a73ab
ANAND S <135801803+anandsundaramoorthysa@users.noreply.github.com>
2026-07-29 22:36:04 +0530

    chore: sync index.ts with main to enable clean merge

--- src/data/schemes/index.ts
+++ src/data/schemes/index.ts
@@ -8,7 +8,6 @@ import { aictePragatiScholarship } from "./aicte-pragati-scholarship";
 import { aicteSakshamDegree } from "./aicte-saksham-degree";
 import { aicteSakshamScholarshipScheme } from "./aicte-saksham-scholarship-scheme";
 import { aicteYashasviScholarship } from "./aicte-yashasvi-scholarship";
-import { biharStudentCreditCardScheme } from "./bihar-student-credit-card-scheme";
 import { bsrFellowshipSchemeForScienceStudentsUgc } from "./bsr-fellowship-scheme-for-science-students-ugc";
 import { deepikaStudentScholarshipProgramme } from "./deepika-student-scholarship-programme";
 import { inspireScholarshipForHigherEducation } from "./inspire-scholarship-for-higher-education";
@@ -27,7 +26,6 @@ export const SCHEMES: Scheme[] = [
   aicteSakshamDegree,
   aicteSakshamScholarshipScheme,
   aicteYashasviScholarship,
-  biharStudentCreditCardScheme,
   bsrFellowshipSchemeForScienceStudentsUgc,
   deepikaStudentScholarshipProgramme,
   inspireScholarshipForHigherEducation,
