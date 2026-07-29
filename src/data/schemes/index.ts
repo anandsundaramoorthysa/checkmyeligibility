71c454d0eff4df8c3c1a15da9c3ae4b37c94af47
ANAND S <135801803+anandsundaramoorthysa@users.noreply.github.com>
2026-07-29 22:31:03 +0530

    chore: sync index.ts with main to enable clean merge

--- src/data/schemes/index.ts
+++ src/data/schemes/index.ts
@@ -14,7 +14,6 @@ import { inspireScholarshipForHigherEducation } from "./inspire-scholarship-for-
 import { jaganannaVidyaDeevenaFeeReimbursement } from "./jagananna-vidya-deevena-fee-reimbursement";
 import { mukhyamantriGyanProtsahanYojanaChhattisgarh } from "./mukhyamantri-gyan-protsahan-yojana-chhattisgarh";
 import { nationalOverseasScholarshipForPersonsWithDisabilities } from "./national-overseas-scholarship-for-persons-with-disabilities";
-import { nationalScholarshipForPersonsWithDisabilities } from "./national-scholarship-for-persons-with-disabilities";
 import { punjabBocwBoardStipendScholarshipScheme } from "./punjab-bocw-board-stipend-scholarship-scheme";
 import { theBabuJagjivanRamHostelSchemeBjrcy } from "./the-babu-jagjivan-ram-hostel-scheme-bjrcy";
 import { vikramadityaScholarshipSchemeMadhyaPradesh } from "./vikramaditya-scholarship-scheme-madhya-pradesh";
@@ -33,7 +32,6 @@ export const SCHEMES: Scheme[] = [
   jaganannaVidyaDeevenaFeeReimbursement,
   mukhyamantriGyanProtsahanYojanaChhattisgarh,
   nationalOverseasScholarshipForPersonsWithDisabilities,
-  nationalScholarshipForPersonsWithDisabilities,
   punjabBocwBoardStipendScholarshipScheme,
   theBabuJagjivanRamHostelSchemeBjrcy,
   vikramadityaScholarshipSchemeMadhyaPradesh,
