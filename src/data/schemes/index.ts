db4f81ce95309b0efc4ca1918ed37dfe4bc02ab4
ANAND S <135801803+anandsundaramoorthysa@users.noreply.github.com>
2026-07-29 22:40:50 +0530

    chore: sync index.ts with main to enable clean merge

--- src/data/schemes/index.ts
+++ src/data/schemes/index.ts
@@ -10,7 +10,6 @@ import { aicteSakshamScholarshipScheme } from "./aicte-saksham-scholarship-schem
 import { aicteYashasviScholarship } from "./aicte-yashasvi-scholarship";
 import { bsrFellowshipSchemeForScienceStudentsUgc } from "./bsr-fellowship-scheme-for-science-students-ugc";
 import { deepikaStudentScholarshipProgramme } from "./deepika-student-scholarship-programme";
-import { drAmbedkarPostMatricScholarshipEbc } from "./dr-ambedkar-post-matric-scholarship-ebc";
 import { inspireScholarshipForHigherEducation } from "./inspire-scholarship-for-higher-education";
 import { jaganannaVidyaDeevenaFeeReimbursement } from "./jagananna-vidya-deevena-fee-reimbursement";
 import { mukhyamantriGyanProtsahanYojanaChhattisgarh } from "./mukhyamantri-gyan-protsahan-yojana-chhattisgarh";
@@ -29,7 +28,6 @@ export const SCHEMES: Scheme[] = [
   aicteYashasviScholarship,
   bsrFellowshipSchemeForScienceStudentsUgc,
   deepikaStudentScholarshipProgramme,
-  drAmbedkarPostMatricScholarshipEbc,
   inspireScholarshipForHigherEducation,
   jaganannaVidyaDeevenaFeeReimbursement,
   mukhyamantriGyanProtsahanYojanaChhattisgarh,
