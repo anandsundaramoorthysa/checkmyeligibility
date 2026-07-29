0e12d296565992a45c9437f18b8eaf4de2f5f351
ANAND S <135801803+anandsundaramoorthysa@users.noreply.github.com>
2026-07-29 22:49:48 +0530

    chore: sync index.ts with main to enable clean merge

--- src/data/schemes/index.ts
+++ src/data/schemes/index.ts
@@ -1,7 +1,6 @@
 // AUTO-GENERATED — do not edit. Run `pnpm collect-schemes` to rebuild.
 import type { Scheme } from "@/lib/types";
 
-import { aictMitacsGlobalinkResearchInternship } from "./aict-mitacs-globalink-research-internship";
 import { aicteDistinguishedChairProfessorFellowship } from "./aicte-distinguished-chair-professor-fellowship";
 import { aicteDoctoralFellowshipAdf } from "./aicte-doctoral-fellowship-adf";
 import { aicteIdeaLab } from "./aicte-idea-lab";
@@ -9,89 +8,17 @@ import { aictePragatiScholarship } from "./aicte-pragati-scholarship";
 import { aicteSakshamDegree } from "./aicte-saksham-degree";
 import { aicteSakshamScholarshipScheme } from "./aicte-saksham-scholarship-scheme";
 import { aicteYashasviScholarship } from "./aicte-yashasvi-scholarship";
-import { anrfNationalPostdoctoralFellowship } from "./anrf-national-postdoctoral-fellowship";
-import { anrfPmEcrgEarlyCareerResearchGrant } from "./anrf-pm-ecrg-early-career-research-grant";
-import { biharStudentCreditCardScheme } from "./bihar-student-credit-card-scheme";
 import { bsrFellowshipSchemeForScienceStudentsUgc } from "./bsr-fellowship-scheme-for-science-students-ugc";
-import { canaraBankAmbedkarVidyaJyothi } from "./canara-bank-ambedkar-vidya-jyothi";
-import { cbseUdaanScheme } from "./cbse-udaan-scheme";
-import { centralVigilanceCommissionInternshipScheme } from "./central-vigilance-commission-internship-scheme";
-import { creditGuaranteeFundSchemeForEducationLoans } from "./credit-guarantee-fund-scheme-for-education-loans";
-import { csirResearchAssociateship } from "./csir-research-associateship";
-import { dbtRamalingaswamiReentryFellowship } from "./dbt-ramalingaswami-reentry-fellowship";
 import { deepikaStudentScholarshipProgramme } from "./deepika-student-scholarship-programme";
-import { delhiBocwEducationAssistance } from "./delhi-bocw-education-assistance";
-import { drAmbedkarPostMatricScholarshipEbc } from "./dr-ambedkar-post-matric-scholarship-ebc";
-import { drPanjabraoDeshmukhHostelAllowance } from "./dr-panjabrao-deshmukh-hostel-allowance";
-import { evnarayanPreMatricScholarshipScheme } from "./evnarayan-pre-matric-scholarship-scheme";
-import { financialSupportSchemeDelhiUniversity } from "./financial-support-scheme-delhi-university";
-import { foreignStudyLoanSchemeGueedc } from "./foreign-study-loan-scheme-gueedc";
-import { gaonKiBetiYojanaMadhyaPradesh } from "./gaon-ki-beti-yojana-madhya-pradesh";
-import { gpatScholarship } from "./gpat-scholarship";
-import { haryanaBocwScholarshipProfessionalTechnicalCourses } from "./haryana-bocw-scholarship-professional-technical-courses";
-import { higherEducationAssistanceSchemeGujarat } from "./higher-education-assistance-scheme-gujarat";
-import { idfcFirstBankEngineeringScholarshipProgram } from "./idfc-first-bank-engineering-scholarship-program";
-import { ietIndiaScholarshipAward } from "./iet-india-scholarship-award";
-import { indianOilAcademicScholarshipScheme } from "./indian-oil-academic-scholarship-scheme";
 import { inspireScholarshipForHigherEducation } from "./inspire-scholarship-for-higher-education";
-import { instrumentalAssistanceMedicalEngineeringDiplomaStudentsSebcGujarat } from "./instrumental-assistance-medical-engineering-diploma-students-sebc-gujarat";
-import { isiStipendSchemeUgPgDiploma } from "./isi-stipend-scheme-ug-pg-diploma";
 import { jaganannaVidyaDeevenaFeeReimbursement } from "./jagananna-vidya-deevena-fee-reimbursement";
-import { jkTyreShikshaSarthiScholarship } from "./jk-tyre-shiksha-sarthi-scholarship";
-import { kanyaSakshartaProtsahanYojana } from "./kanya-saksharta-protsahan-yojana";
-import { meritCumMeansScholarshipForMinorityStudents } from "./merit-cum-means-scholarship-for-minority-students";
 import { mukhyamantriGyanProtsahanYojanaChhattisgarh } from "./mukhyamantri-gyan-protsahan-yojana-chhattisgarh";
-import { mukhyamantriGyanSadhanaMeritScholarshipScheme } from "./mukhyamantri-gyan-sadhana-merit-scholarship-scheme";
-import { mukhyamantriMedhaviVidyarthiYojana } from "./mukhyamantri-medhavi-vidyarthi-yojana";
-import { mukhyamantriYuvaSwavalambanYojanaGujarat } from "./mukhyamantri-yuva-swavalamban-yojana-gujarat";
-import { naiUdaanScheme } from "./nai-udaan-scheme";
-import { namoSaraswatiVigyanSadhanaYojana } from "./namo-saraswati-vigyan-sadhana-yojana";
-import { narotamSekhsariaPostgraduateScholarship } from "./narotam-sekhsaria-postgraduate-scholarship";
-import { nationalFellowshipForObcStudents } from "./national-fellowship-for-obc-students";
-import { nationalFellowshipForStudentsWithDisabilities } from "./national-fellowship-for-students-with-disabilities";
 import { nationalOverseasScholarshipForPersonsWithDisabilities } from "./national-overseas-scholarship-for-persons-with-disabilities";
-import { nationalOverseasScholarshipSt } from "./national-overseas-scholarship-st";
-import { nationalSchemeOfIncentiveToGirlsForSecondaryEducationNsigse } from "./national-scheme-of-incentive-to-girls-for-secondary-education-nsigse";
-import { nationalScholarshipForPersonsWithDisabilities } from "./national-scholarship-for-persons-with-disabilities";
-import { nayaSaveraFreeCoachingScheme } from "./naya-savera-free-coaching-scheme";
-import { nbfcdEducationLoan } from "./nbfcd-education-loan";
-import { necMeritScholarship } from "./nec-merit-scholarship";
-import { nhfdcScholarshipScheme } from "./nhfdc-scholarship-scheme";
-import { odishaNirmanShramikKalyanYojana } from "./odisha-nirman-shramik-kalyan-yojana";
-import { oecPreMatricEducationalAssistance } from "./oec-pre-matric-educational-assistance";
-import { opJindalEngineeringManagementScholarshipOpjems } from "./op-jindal-engineering-management-scholarship-opjems";
-import { padhoPardeshScheme } from "./padho-pardesh-scheme";
-import { perarignarAnnaMemorialAward } from "./perarignar-anna-memorial-award";
-import { pmUspCentralSectorSchemeOfScholarship } from "./pm-usp-central-sector-scheme-of-scholarship";
-import { pmYasasviScheme } from "./pm-yasasvi-scheme";
-import { postDoctoralFellowshipToWomenCandidates } from "./post-doctoral-fellowship-to-women-candidates";
-import { postGraduateScholarshipForUniversityRankHolders } from "./post-graduate-scholarship-for-university-rank-holders";
-import { postGraduateScholarshipsForProfessionalCoursesForScStCandidates } from "./post-graduate-scholarships-for-professional-courses-for-sc-st-candidates";
-import { postMatricScholarshipScheduledTribeStudents } from "./post-matric-scholarship-scheduled-tribe-students";
-import { postMatricScholarshipStudentsWithDisabilities } from "./post-matric-scholarship-students-with-disabilities";
-import { preMatricScholarshipForStudentsWithDisabilities } from "./pre-matric-scholarship-for-students-with-disabilities";
-import { preMatricScholarshipSchemeForObcStudents } from "./pre-matric-scholarship-scheme-for-obc-students";
-import { primeMinistersFellowshipDoctoralResearch } from "./prime-ministers-fellowship-doctoral-research";
 import { punjabBocwBoardStipendScholarshipScheme } from "./punjab-bocw-board-stipend-scholarship-scheme";
-import { rajivGandhiNationalFellowshipSc } from "./rajiv-gandhi-national-fellowship-sc";
-import { rblBankShikshaScholarship } from "./rbl-bank-shiksha-scholarship";
-import { reimbursementOfCollegeFeesForScStudentsInProfessionalColleges } from "./reimbursement-of-college-fees-for-sc-students-in-professional-colleges";
-import { ruralGirlsIncentiveScheme } from "./rural-girls-incentive-scheme";
-import { shreyasUmbrellaScheme } from "./shreyas-umbrella-scheme";
-import { sitaramJindalScholarship } from "./sitaram-jindal-scholarship";
-import { specialScholarshipSchemeForJKAndLadakhSssjkl } from "./special-scholarship-scheme-for-j-k-and-ladakh-sssjkl";
-import { swamiVivekanandaMeritCumMeansScholarshipWestBengal } from "./swami-vivekananda-merit-cum-means-scholarship-west-bengal";
-import { tamilNaduFirstGraduateTuitionFeeConcession } from "./tamil-nadu-first-graduate-tuition-fee-concession";
-import { tamilNaduPostMatricScholarshipBcMbcDnc } from "./tamil-nadu-post-matric-scholarship-bc-mbc-dnc";
-import { tecInternshipScheme } from "./tec-internship-scheme";
-import { telanganaEpassPostMatricScholarship } from "./telangana-epass-post-matric-scholarship";
 import { theBabuJagjivanRamHostelSchemeBjrcy } from "./the-babu-jagjivan-ram-hostel-scheme-bjrcy";
-import { topClassEducationSchemeForScStudents } from "./top-class-education-scheme-for-sc-students";
 import { vikramadityaScholarshipSchemeMadhyaPradesh } from "./vikramaditya-scholarship-scheme-madhya-pradesh";
-import { wiseKiranScheme } from "./wise-kiran-scheme";
 
 export const SCHEMES: Scheme[] = [
-  aictMitacsGlobalinkResearchInternship,
   aicteDistinguishedChairProfessorFellowship,
   aicteDoctoralFellowshipAdf,
   aicteIdeaLab,
@@ -99,84 +26,13 @@ export const SCHEMES: Scheme[] = [
   aicteSakshamDegree,
   aicteSakshamScholarshipScheme,
   aicteYashasviScholarship,
-  anrfNationalPostdoctoralFellowship,
-  anrfPmEcrgEarlyCareerResearchGrant,
-  biharStudentCreditCardScheme,
   bsrFellowshipSchemeForScienceStudentsUgc,
-  canaraBankAmbedkarVidyaJyothi,
-  cbseUdaanScheme,
-  centralVigilanceCommissionInternshipScheme,
-  creditGuaranteeFundSchemeForEducationLoans,
-  csirResearchAssociateship,
-  dbtRamalingaswamiReentryFellowship,
   deepikaStudentScholarshipProgramme,
-  delhiBocwEducationAssistance,
-  drAmbedkarPostMatricScholarshipEbc,
-  drPanjabraoDeshmukhHostelAllowance,
-  evnarayanPreMatricScholarshipScheme,
-  financialSupportSchemeDelhiUniversity,
-  foreignStudyLoanSchemeGueedc,
-  gaonKiBetiYojanaMadhyaPradesh,
-  gpatScholarship,
-  haryanaBocwScholarshipProfessionalTechnicalCourses,
-  higherEducationAssistanceSchemeGujarat,
-  idfcFirstBankEngineeringScholarshipProgram,
-  ietIndiaScholarshipAward,
-  indianOilAcademicScholarshipScheme,
   inspireScholarshipForHigherEducation,
-  instrumentalAssistanceMedicalEngineeringDiplomaStudentsSebcGujarat,
-  isiStipendSchemeUgPgDiploma,
   jaganannaVidyaDeevenaFeeReimbursement,
-  jkTyreShikshaSarthiScholarship,
-  kanyaSakshartaProtsahanYojana,
-  meritCumMeansScholarshipForMinorityStudents,
   mukhyamantriGyanProtsahanYojanaChhattisgarh,
-  mukhyamantriGyanSadhanaMeritScholarshipScheme,
-  mukhyamantriMedhaviVidyarthiYojana,
-  mukhyamantriYuvaSwavalambanYojanaGujarat,
-  naiUdaanScheme,
-  namoSaraswatiVigyanSadhanaYojana,
-  narotamSekhsariaPostgraduateScholarship,
-  nationalFellowshipForObcStudents,
-  nationalFellowshipForStudentsWithDisabilities,
   nationalOverseasScholarshipForPersonsWithDisabilities,
-  nationalOverseasScholarshipSt,
-  nationalSchemeOfIncentiveToGirlsForSecondaryEducationNsigse,
-  nationalScholarshipForPersonsWithDisabilities,
-  nayaSaveraFreeCoachingScheme,
-  nbfcdEducationLoan,
-  necMeritScholarship,
-  nhfdcScholarshipScheme,
-  odishaNirmanShramikKalyanYojana,
-  oecPreMatricEducationalAssistance,
-  opJindalEngineeringManagementScholarshipOpjems,
-  padhoPardeshScheme,
-  perarignarAnnaMemorialAward,
-  pmUspCentralSectorSchemeOfScholarship,
-  pmYasasviScheme,
-  postDoctoralFellowshipToWomenCandidates,
-  postGraduateScholarshipForUniversityRankHolders,
-  postGraduateScholarshipsForProfessionalCoursesForScStCandidates,
-  postMatricScholarshipScheduledTribeStudents,
-  postMatricScholarshipStudentsWithDisabilities,
-  preMatricScholarshipForStudentsWithDisabilities,
-  preMatricScholarshipSchemeForObcStudents,
-  primeMinistersFellowshipDoctoralResearch,
   punjabBocwBoardStipendScholarshipScheme,
-  rajivGandhiNationalFellowshipSc,
-  rblBankShikshaScholarship,
-  reimbursementOfCollegeFeesForScStudentsInProfessionalColleges,
-  ruralGirlsIncentiveScheme,
-  shreyasUmbrellaScheme,
-  sitaramJindalScholarship,
-  specialScholarshipSchemeForJKAndLadakhSssjkl,
-  swamiVivekanandaMeritCumMeansScholarshipWestBengal,
-  tamilNaduFirstGraduateTuitionFeeConcession,
-  tamilNaduPostMatricScholarshipBcMbcDnc,
-  tecInternshipScheme,
-  telanganaEpassPostMatricScholarship,
   theBabuJagjivanRamHostelSchemeBjrcy,
-  topClassEducationSchemeForScStudents,
   vikramadityaScholarshipSchemeMadhyaPradesh,
-  wiseKiranScheme,
 ];
