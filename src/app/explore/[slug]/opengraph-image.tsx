7b2804f6a0bbd88110185d269eb5aef2594ecd2f
Anand Sundaramoorthy SA <sanand03072005@gmail.com>
2026-08-12 23:16:01 +0530

    fix(build): remove edge runtime from scheme OG image to allow static generation

--- src/app/explore/[slug]/opengraph-image.tsx
+++ src/app/explore/[slug]/opengraph-image.tsx
@@ -2,7 +2,6 @@ import { ImageResponse } from "next/og";
 import { getSchemeBySlug } from "@/lib/data";
 import { SITE } from "@/lib/site";
 
-export const runtime = "edge";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
